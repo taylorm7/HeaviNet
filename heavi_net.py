e23b7248a22e5de4bad7915c817ec8ef13115542
Sable <TaylorM7@vt.edu>
2017-05-20 17:09:07 -0400

    Regression training

--- heavi_net.py
+++ heavi_net.py
@@ -1,6 +1,37 @@
 import tensorflow as tf
-
-hello = tf.constant('Hello, TensorFlow!')
+import numpy as np
+'''
+a = tf.constant(4, name="input_a")
+b = tf.constant(3, name="input_b")
+c = tf.add(a,b,name="add_c")
+d = tf.multiply(a,b,name="mul_d")
+e = tf.multiply(c,d,name="mul_e")
 sess = tf.Session()
+print(sess.run(e))
+'''
 
-print(sess.run(hello))
+# Model parameters
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+# Model input and output
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+# training data
+x_train = [1,2,3,4]
+y_train = [0,-1,-2,-3]
+# training loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init) # reset values to wrong
+for i in range(1000):
+  sess.run(train, {x:x_train, y:y_train})
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x:x_train, y:y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
