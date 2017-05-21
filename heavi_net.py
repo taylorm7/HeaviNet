a2472f9a2de3bf33f52f0b7a7cbb0f40d0b41fdb
Sable <TaylorM7@vt.edu>
2017-05-21 14:01:57 -0400

    Completed Feedforward computational graph

--- heavi_net.py
+++ heavi_net.py
@@ -19,9 +19,34 @@ x = tf.placeholder('float', [None,784])
 y = tf.placeholder('float')
 
 def neural_network_model(data):
-    # input_data * weights + biases
+
+    #create variables for the hidden layers
     hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784,n_nodes_hl1])),
                       'biases':tf.Variable(tf.random_normal(n_nodes_hl1))}
+
+    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1,n_nodes_hl2])),
+                      'biases':tf.Variable(tf.random_normal(n_nodes_hl2))}
+
+    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2,n_nodes_hl3])),
+                      'biases':tf.Variable(tf.random_normal(n_nodes_hl3))}
+
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3,n_classes])),
+                    'biases':tf.Variable(tf.random_normal(n_classes))}
+
+    # input_data * weights + biases
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases']
+
+    return output
+
 
 '''
 # Model parameters
