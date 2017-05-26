e82ec29eee9ccceb25cd428dbd6a18844d52b2d6
Sable <TaylorM7@vt.edu>
2017-05-26 16:07:56 -0400

    CSV import

--- heavi_net.py
+++ heavi_net.py
@@ -1,101 +1,10 @@
 import tensorflow as tf
-
-from tensorflow.examples.tutorials.mnist import input_data
-
-#10 classes, 0-9 using one_hot ie. 0 = [1,0,0...]
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
-
-n_classes = 10
-batch_size = 100 #100 images per batch
-
-#height x width
-x = tf.placeholder('float', [None, 784]) 
-#shape is 'none:squashed' by 28*28, not necessary to include shape [h,w] but this is helpful to give an error if something doesn't match dimensions
-
-y = tf.placeholder('float')
-
-def neural_network_model(data):
-    #create variables for the hidden layers
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])), 'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
-
-    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])), 'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
-
-    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])), 'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
-
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])), 'biases':tf.Variable(tf.random_normal([n_classes]))}
-    
-    # input_data * weights + biases
-    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
-    l1 = tf.nn.relu(l1)
-
-    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
-    l2 = tf.nn.relu(l2)
-
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l3)
-
-    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
-    return output
-
-def train_neural_network(x):
-    prediction = neural_network_model(x)
-    
-
-    #learning_rate = 0.01
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
-
-    #clycles of feed forward + backpropigation
-    hm_epochs = 1
-
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        
-        #training data
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
-            for _ in range( int(mnist.train.num_examples/batch_size) ):
-                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x, y: epoch_y})
-                epoch_loss += c
-            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
-
-        #now the weights have been optimized, and we compare preditions
-        correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
-        #flot of the correctness
-        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-        print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
-
-train_neural_network(x)
+import numpy as np
 
 
-'''
-# Model parameters
-W = tf.Variable([.3], tf.float32)
-b = tf.Variable([-.3], tf.float32)
-# Model input and output
-x = tf.placeholder(tf.float32)
-linear_model = W * x + b
-y = tf.placeholder(tf.float32)
-# loss
-loss = tf.reduce_sum(tf.square(linear_model - y)) # sum of the squares
-# optimizer
-optimizer = tf.train.GradientDescentOptimizer(0.01)
-train = optimizer.minimize(loss)
-# training data
-x_train = [1,2,3,4]
-y_train = [0,-1,-2,-3]
-# training loop
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init) # reset values to wrong
-for i in range(1000):
-  sess.run(train, {x:x_train, y:y_train})
+mat = np.loadtxt(open("test.csv", "rb"), delimiter=",", skiprows=1)
 
-# evaluate training accuracy
-curr_W, curr_b, curr_loss  = sess.run([W, b, loss], {x:x_train, y:y_train})
-print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
-'''
+print (mat)
+print(mat.shape)
+
+print("End")
