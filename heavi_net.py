7437e3a682d3ed9ce05425161da269d635b65636
Sable <TaylorM7@vt.edu>
2017-05-27 15:24:25 -0400

    Beginning Music Matrix Formatting

--- heavi_net.py
+++ heavi_net.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import scipy.io as sio
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 
 matrix_file= sio.loadmat('/home/sable/AudioFiltering/Testing/test.mat')
@@ -12,4 +14,11 @@ print(mat[1:100,0])
 print(mat.shape)
 
 
-print("End")
+from tensorflow.examples.tutorials.mnist import input_data
+data = input_data.read_data_sets("data/MNIST/", one_hot=True)
+
+#create set of single values for data.test
+data.test.cls = np.array([label.argmax() for label in data.test.labels])
+
+print(data.test.labels.shape)
+print(data.test.cls.shape)
