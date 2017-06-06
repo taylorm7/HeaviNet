cde576265fceb3dea245040890e51ab0b914dc06
Sable <TaylorM7@vt.edu>
2017-06-06 15:44:01 -0400

    CNN beginning development

--- cnn_heavinet.py
+++ cnn_heavinet.py
@@ -2,18 +2,18 @@ import tensorflow as tf
 import numpy as np
 import scipy.io as sio
 import matplotlib.pyplot as plt
-from tensorflow.contrib import rnn
+#from sklearn.metrics import confusion_matrix
 from random import randint
 
 clip_size = 32
-n_clips = 32
+n_clips = 128
 
 num_classes = 256
+batch_size = 512
 
-batch_size = 128
-
-rnn_size = 128
-
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
 
 def make_onehot(onehot_values, onehot_classes):
     onehot_matrix = np.zeros((onehot_values.size, onehot_classes))
@@ -37,24 +37,6 @@ def batch(iterable, start, batches=0):
     b_onehot = make_onehot(b_y, num_classes)
     return b_clip, b_onehot, b_y 
 
-def batch_r3(iterable, start, batches):
-    b_clip = np.zeros((0, n_clips, clip_size))
-    b_onehot = np.zeros((0 , n_clips,  num_classes))
-    b_y =np.zeros((0, n_clips))
-    
-    if start + clip_size + batches - 1 > len(iterable):
-        return b_clip, b_onehot, b_y 
-        
-    for i in range(batches):
-        clip, onehot, y = batch(iterable, start, n_clips)
-        b_clip = np.append(b_clip, clip.reshape((1,n_clips, clip_size)), axis = 0 )
-        b_onehot = np.append(b_onehot, onehot.reshape((1, n_clips, num_classes)),axis=0)
-        b_y = np.append(b_y, y.reshape((1,n_clips)), axis=0)
-    return b_clip, b_onehot, b_y 
-    
-    
-
-
 matrix_file= sio.loadmat('/home/sable/AudioFiltering/Testing/test.mat')
 mat = matrix_file['data']
 print type(mat)
@@ -62,21 +44,34 @@ print type(mat)
 #print(mat[1:100,0], mat.shape)
 
 # input vector
-x = tf.placeholder(tf.float32, [None, n_clips, clip_size])
-y_true = tf.placeholder(tf.float32)
-y_true_cls = tf.placeholder(tf.int64)
+x = tf.placeholder(tf.float32, [None, clip_size])
+y_true = tf.placeholder(tf.float32, [None, num_classes])
+y_true_cls = tf.placeholder(tf.int64, [None])
 
-def neural_network_model(x):
-    layer = {'weights':tf.Variable(tf.random_normal([rnn_size, num_classes])),
-             'biases':tf.Variable(tf.random_normal([num_classes]))}
-    #x = tf.transpose(x, [-1,0,2])
-    x = tf.reshape(x, [-1, n_clips])
-    x = tf.split(x, n_clips, 0)
+def neural_network_model(data):
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([clip_size, n_nodes_hl1])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
-    lstm_cell = rnn.BasicLSTMCell(rnn_size)
-    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
-    output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
+    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, num_classes])),
+                    'biases':tf.Variable(tf.random_normal([num_classes]))}
+
+
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
+    l1 = tf.nn.tanh(l1)
+
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.nn.tanh(l2)
+
+    l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.nn.tanh(l3)
+
+    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
 
     return output
 
@@ -89,7 +84,7 @@ y_pred = tf.nn.softmax(logits)
 y_pred_cls = tf.argmax(y_pred, dimension=1)
 
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y_pred, labels=y_true)
 cost = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(cost)
 #optimizer = tf.train.AdamOptimizer().minimize(cost)
@@ -125,8 +120,7 @@ def optimize(epochs, iterations=len(mat) ):
             #x_batch, y_true_batch = data.train.next_batch(batch_size)
             x_batch, y_onehot_batch, y_class_batch = batch(mat,start, batch_size)
             
-            x_batch, _, _ = batch_r3(mat, start, batch_size)
-            print(start, "x batch", x_batch.shape, "y batch", y_onehot_batch.shape, type(x_batch), type(y_onehot_batch) )
+            #print(start, "x batch", x_batch.shape, "y batch", y_onehot_batch.shape, type(x_batch), type(y_onehot_batch) )
             # Put the batch into a dict with the proper names
             # for placeholder variables in the TensorFlow graph.
             # Note that the placeholder for y_true_cls is not set
@@ -134,25 +128,21 @@ def optimize(epochs, iterations=len(mat) ):
             feed_dict_train = {x: x_batch,
                                y_true: y_onehot_batch,
                                y_true_cls: y_class_batch }
-            
-            #print x_batch.shape, "x batch\n", x_batch
-            #print y_onehot_batch.shape, "y true batch\n", y_onehot_batch
+
             # Run the optimizer using this batch of training data.
             # TensorFlow assigns the variables in feed_dict_train
             # to the placeholder variables and then runs the optimizer.
-            o, c, cor, y = session.run([optimizer, cost, correct_prediction, y_pred_cls], feed_dict=feed_dict_train)
+            o, c, cor = session.run([optimizer, cost, correct_prediction], feed_dict=feed_dict_train)
             epoch_correct += np.sum(cor)
             epoch_total += cor.size
             epoch_loss += c
-            print y.shape, "y predicted", y
-            #print correct_prediction
-        print epoch_total,":epoch", i, "completed w/ loss", epoch_loss, "correct", epoch_correct, "and percentage" , 100.0 * float(epoch_correct) / float(epoch_total)
+        print epoch_total,":epoch", i, "completed w/ loss", epoch_loss, "correct", epoch_correct, "and percentage" , float(epoch_correct) / float(epoch_total)
 
-        #test_index = randint(0, len(mat)-1000)
-        #print "test at", test_index
-        #test_clip, test_y_onehot, test_y = batch(mat, test_index, 10000)
-        #feed_dict_test = {x: test_clip, y_true: test_y_onehot, y_true_cls: test_y} 
-        #print_accuracy(feed_dict_test)
+        test_index = randint(0, len(mat)-1000)
+        print "test at", test_index
+        test_clip, test_y_onehot, test_y = batch(mat, test_index, 10000)
+        feed_dict_test = {x: test_clip, y_true: test_y_onehot, y_true_cls: test_y} 
+        print_accuracy(feed_dict_test)
 
 
 def predict():
@@ -167,19 +157,11 @@ def create(song_seed, length):
         song_seed = np.append(song_seed, song_new_y, axis = 0)
     return song_seed
 
-#song, _, _ = batch(mat, 20000, 1)
-#song = song.reshape( (clip_size) )
+song, _, _ = batch(mat, 20000, 1)
+song = song.reshape( (clip_size) )
 
-optimize(3, batch_size)
-#song = create(song, 100)
-#print song, song.shape
+optimize(1)
+song = create(song, 100)
+print song, song.shape
 
-
-'''
-x_batch, y_onehot_batch, y_class_batch = batch_r3(mat,10, batch_size)
-            
-print x_batch.shape, "x batch\n", x_batch
-print y_onehot_batch.shape, "y onehot batch/n", y_onehot_batch
-print y_class_batch.shape, "y class batch\n", y_class_batch
-'''
 session.close()
