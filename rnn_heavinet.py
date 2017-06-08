1bafac81c27c10bdef96883e64d91d8e74ee5670
Sable <TaylorM7@vt.edu>
2017-06-08 12:58:35 -0400

    RNN working with non-onehot values

--- rnn_heavinet.py
+++ rnn_heavinet.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from tensorflow.contrib import rnn
 from random import randint
 
-clip_size = 32
-n_clips = 32
+clip_size = 32*4*120
+n_clips = 1
 
 num_classes = 256
 
@@ -20,20 +20,20 @@ def make_onehot(onehot_values, onehot_classes):
     onehot_matrix[np.arange(onehot_values.size),onehot_values] = 1
     return onehot_matrix
 
-def batch(iterable, start, batches=0):
-    if start + clip_size + 1 > len(iterable):
-        return np.zeros((0, clip_size)), np.zeros((0 , num_classes)), np.zeros(0)
-    b_clip = iterable[start:start+clip_size, :]
-    b_y = iterable[start+clip_size, 0]
+def batch(iterable, start, batches=0, size=clip_size):
+    if start + size + 1 > len(iterable):
+        return np.zeros((0, size)), np.zeros((0 , num_classes)), np.zeros(0)
+    b_clip = iterable[start:start+size, :]
+    b_y = iterable[start+size, 0]
     for i in range(1,batches):
-        if start+clip_size+i+1 <= len(iterable):
-            b_clip = np.append(b_clip, iterable[start+i:start+clip_size+i, :], axis = 0) 
-            b_y = np.append(b_y, iterable[start+i+clip_size, 0])
+        if start+size+i+1 <= len(iterable):
+            b_clip = np.append(b_clip, iterable[start+i:start+size+i, :], axis = 0) 
+            b_y = np.append(b_y, iterable[start+i+size, 0])
 
-    batch_overflow = len(b_clip) % clip_size
+    batch_overflow = len(b_clip) % size
     if batch_overflow != 0:
         b_clip =  b_clip[:-batch_overflow or None, :]
-    b_clip = b_clip.reshape( (-1, clip_size) )    
+    b_clip = b_clip.reshape( (-1, size) )    
     b_onehot = make_onehot(b_y, num_classes)
     return b_clip, b_onehot, b_y 
 
@@ -62,29 +62,33 @@ print type(mat)
 #print(mat[1:100,0], mat.shape)
 
 # input vector
-x = tf.placeholder(tf.float32, [None, n_clips, clip_size])
+x = tf.placeholder(tf.float32, [None, n_clips])
+x_d3 = tf.reshape(x, (-1, n_clips, 1))
+x_onehot = tf.one_hot(tf.cast(x,tf.int32), num_classes)
+print x_onehot.shape
+print x_d3
 y_true = tf.placeholder(tf.float32)
 y_true_cls = tf.placeholder(tf.int64)
 
-def neural_network_model(x):
+def neural_network_model(data):
     layer = {'weights':tf.Variable(tf.random_normal([rnn_size, num_classes])),
              'biases':tf.Variable(tf.random_normal([num_classes]))}
-    #x = tf.transpose(x, [-1,0,2])
-    x = tf.reshape(x, [-1, n_clips])
-    x = tf.split(x, n_clips, 0)
+    #x = tf.transpose(data, [-1,0,2])
+    data = tf.reshape(data, [-1, n_clips])
+    data = tf.split(data, n_clips, 0)
 
     lstm_cell = rnn.BasicLSTMCell(rnn_size)
-    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
-
+    outputs, states = rnn.static_rnn(lstm_cell, data, dtype=tf.float32)
+    print "rnn outputs", outputs
     output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
-
+    print "output", output
     return output
 
 #weights = tf.Variable(tf.zeros([clip_size, num_classes]))
 #biases = tf.Variable(tf.zeros([num_classes]))
 #logits = tf.matmul(x, weights) + biases
 logits = neural_network_model(x)
-
+print "logits", logits
 y_pred = tf.nn.softmax(logits)
 y_pred_cls = tf.argmax(y_pred, dimension=1)
 
@@ -123,9 +127,8 @@ def optimize(epochs, iterations=len(mat) ):
             # x_batch now holds a batch of images and
             # y_true_batch are the true labels for those images.
             #x_batch, y_true_batch = data.train.next_batch(batch_size)
-            x_batch, y_onehot_batch, y_class_batch = batch(mat,start, batch_size)
+            x_batch, y_onehot_batch, y_class_batch = batch(mat,start, batch_size, n_clips)
             
-            x_batch, _, _ = batch_r3(mat, start, batch_size)
             print(start, "x batch", x_batch.shape, "y batch", y_onehot_batch.shape, type(x_batch), type(y_onehot_batch) )
             # Put the batch into a dict with the proper names
             # for placeholder variables in the TensorFlow graph.
