938cf72454ca4602a34ad5a1b62b2429a7ed8740
Sable <TaylorM7@vt.edu>
2017-06-16 18:42:27 -0400

    Heavinet, util, implemented pickle read/write after formatting

--- heavinet.py
+++ heavinet.py
@@ -1,13 +1,17 @@
 import tensorflow as tf
 import numpy as np
-
 import scipy.io as sio
 
-from util import read_data
-
-data_path = '/home/sable/HeaviNet/data/'
+from util import read_data, name_level, format_level
 
 
-levels = read_data(data_path)
+receptive_field = 5
+n_levels = 7
 
+level_names, ytrue_names = name_level(n_levels)
 
+print level_names, ytrue_names
+
+x_data, ytrue_data = read_data(receptive_field, level_names, ytrue_names, force_read=True)
+
+print x_data['level_4'].size, ytrue_data['ytrue_4'].size
--- util.py
+++ util.py
@@ -1,20 +1,28 @@
 import numpy as np
 import os.path
 import scipy.io
+import cPickle as pkl
+
+def name_level(n_levels):
+    x_name = []
+    ytrue_name = []
+    for i in range(1,n_levels+1):
+        x_name.append(str( "level_" + str(i)))
+        ytrue_name.append(str("ytrue_" + str(i)))
+    return x_name, ytrue_name
+
 
 def format_level(iterable, receptive_field):
-    print type(iterable)
-    
-    indicies = range(receptive_field, -1, -1)
+    indicies = range(receptive_field-1, -1, -1)
     for i in range(len(indicies)):
         indicies[i] = -1* 2**indicies[i]
     indicies.append(0)
-    indicies_pos = range(0, receptive_field+1)
+    indicies_pos = range(0, receptive_field)
     for i in range(len(indicies_pos)):
         indicies_pos[i] = 2**indicies_pos[i]
     
     indicies = indicies + indicies_pos
-
+    
     level = []
     for i in range(len(iterable)):
         l = []
@@ -24,16 +32,27 @@ def format_level(iterable, receptive_field):
     
     return level
 
-def read_data(data_path, force_read=False):
+def read_data(receptive_field, level_names, ytrue_names, force_read=False):
 
-    data = []
+    x_data = {}
+    ytrue_data = {}
 
-    if os.path.isfile(data_path + "matlab_input.npy" ):
-        print "PYC Load file:" + data_path + "matlab_input.npy"
-        data = np.load(data_path + "matlab_input.npy")
+    if os.path.isfile("data/x_ytrue.pkl" ) and force_read==False :
+        print "Pickle Load file x_ytrue.pkl"
+        with open(r"data/x_ytrue.pkl", "rb") as input_file:
+            data_list = pkl.load(input_file)
+        x_data = data_list[0]
+        ytrue_data = data_list[1]
     else:
-        print "Matlab Load file" + data_path +"matlabSong2heavinet.mat" 
-        matlab_input = scipy.io.loadmat(data_path + "matlabSong2heavinet.mat")
-        data = np.append(data, format_level(matlab_input['level_1'], 5))
-        data = np.append(data, format_level(matlab_input['level_2'], 5))
-    print data.shape
+        print "Matlab Load file matlabSong2heavinet.mat" 
+        matlab_input = scipy.io.loadmat("data/matlabSong2heavinet.mat")
+        for name in level_names:
+            x_data[name] = format_level(matlab_input[name], receptive_field)   
+        for name in ytrue_names:
+            ytrue_data[name] = matlab_input[name]
+        
+        data_list = [x_data, ytrue_data]
+        
+        with open(r"data/x_ytrue.pkl", "wb") as output_file:
+            pkl.dump(data_list, output_file)
+    return x_data, ytrue_data
