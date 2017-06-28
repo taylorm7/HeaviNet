d9c6bb809eff4a706abe0ab03f09c29065bd8c52
Sable <TaylorM7@vt.edu>
2017-06-28 13:24:52 -0400

    Conv nn working; refactoring code

--- models.py
+++ models.py
@@ -2,27 +2,186 @@ import tensorflow as tf
 import numpy as np
 import os
 
+# Convolutional layers referenced and modified from:
+# https://github.com/Hvass-Labs/TensorFlow-Tutorials -> 02_Convolutional_Neural_Network.ipynb
+
+def new_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+
+def new_biases(length):
+    return tf.Variable(tf.constant(0.05, shape=[length]))
 
 def nn_layer(input_layer, n_nodes_in, n_nodes, output_layer=False):
     hl_weight = tf.Variable(tf.random_normal([n_nodes_in, n_nodes]))
     hl_bias = tf.Variable(tf.random_normal([n_nodes]))
+
+    layer =  tf.matmul(input_layer, hl_weight) + hl_bias
+
     if(not output_layer):
-        layer = tf.add(tf.matmul(input_layer, hl_weight), hl_bias)
         layer = tf.nn.relu(layer)
-    else:
-        layer =  tf.matmul(input_layer, hl_weight) + hl_bias
     return layer
 
+def new_conv_layer(input,              # The previous layer.
+                   num_input_channels, # Num. channels in prev. layer.
+                   filter_size,        # Width and height of each filter.
+                   num_filters,        # Number of filters.
+                   use_pooling=True):  # Use 2x2 max-pooling.
+
+    # Shape of the filter-weights for the convolution.
+    # This format is determined by the TensorFlow API.
+    shape = [filter_size, filter_size, num_input_channels, num_filters]
+
+    # Create new weights aka. filters with the given shape.
+    weights = new_weights(shape=shape)
+
+    # Create new biases, one for each filter.
+    biases = new_biases(length=num_filters)
+
+    # Create the TensorFlow operation for convolution.
+    # Note the strides are set to 1 in all dimensions.
+    # The first and last stride must always be 1,
+    # because the first is for the image-number and
+    # the last is for the input-channel.
+    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
+    # is moved 2 pixels across the x- and y-axis of the image.
+    # The padding is set to 'SAME' which means the input image
+    # is padded with zeroes so the size of the output is the same.
+    layer = tf.nn.conv2d(input=input,
+                         filter=weights,
+                         strides=[1, 1, 1, 1],
+                         padding='SAME')
+
+    # Add the biases to the results of the convolution.
+    # A bias-value is added to each filter-channel.
+    layer += biases
+
+    # Use pooling to down-sample the image resolution?
+    if use_pooling:
+        # This is 2x2 max-pooling, which means that we
+        # consider 2x2 windows and select the largest value
+        # in each window. Then we move 2 pixels to the next window.
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME')
+
+    # Rectified Linear Unit (ReLU).
+    # It calculates max(x, 0) for each input pixel x.
+    # This adds some non-linearity to the formula and allows us
+    # to learn more complicated functions.
+    layer = tf.nn.relu(layer)
+
+    # Note that ReLU is normally executed before the pooling,
+    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+    # save 75% of the relu-operations by max-pooling first.
+
+    # We return both the resulting layer and the filter-weights
+    # because we will plot the weights later.
+    return layer, weights
+
+def flatten_layer(layer):
+    # Get the shape of the input layer.
+    layer_shape = layer.get_shape()
+
+    # The shape of the input layer is assumed to be:
+    # layer_shape == [num_images, img_height, img_width, num_channels]
+
+    # The number of features is: img_height * img_width * num_channels
+    # We can use a function from TensorFlow to calculate this.
+    num_features = layer_shape[1:4].num_elements()
+    
+    # Reshape the layer to [num_images, num_features].
+    # Note that we just set the size of the second dimension
+    # to num_features and the size of the first dimension to -1
+    # which means the size in that dimension is calculated
+    # so the total size of the tensor is unchanged from the reshaping.
+    layer_flat = tf.reshape(layer, [-1, num_features])
+
+    # The shape of the flattened layer is now:
+    # [num_images, img_height * img_width * num_channels]
+
+    # Return both the flattened layer and the number of features.
+    return layer_flat, num_features
+
+def new_fc_layer(input,          # The previous layer.
+                 num_inputs,     # Num. inputs from prev. layer.
+                 num_outputs,    # Num. outputs.
+                 use_relu=True): # Use Rectified Linear Unit (ReLU)?
+
+    # Create new weights and biases.
+    weights = new_weights(shape=[num_inputs, num_outputs])
+    biases = new_biases(length=num_outputs)
+
+    # Calculate the layer as the matrix multiplication of
+    # the input and weights, and then add the bias-values.
+    layer = tf.matmul(input, weights) + biases
+
+    # Use ReLU?
+    if use_relu:
+        layer = tf.nn.relu(layer)
+
+    return layer
+
+
+
+
 class Model(object):
-    def perceptron_nn(self, data, n_input_classes, n_target_classes, clip_size, n_nodes):
+    def perceptron_nn(self, input, n_input_classes, n_target_classes, clip_size, n_nodes):
         layers = []
         for i, nodes in enumerate( n_nodes ):
             if i == 0:
-                layers.append(nn_layer(data, n_input_classes*clip_size, n_nodes[0]))
+                layers.append(nn_layer(input, n_input_classes*clip_size, n_nodes[0]))
             else:
                 layers.append(nn_layer(layers[i-1], n_nodes[i-1], n_nodes[i]))
         output_layer = nn_layer(layers[-1], n_nodes[-1], n_target_classes, output_layer=True)
         return output_layer
+
+    def nn_fc_layers(self, input, n_input_classes, n_target_classes, n_nodes):
+        layers = []
+        for i, nodes in enumerate( n_nodes ):
+            if i == 0:
+                layers.append(new_fc_layer(input, n_input_classes, n_nodes[0], use_relu=True))
+            else:
+                layers.append(new_fc_layer(layers[i-1], n_nodes[i-1], n_nodes[i], use_relu=True))
+
+        output_layer = nn_layer(layers[-1], n_nodes[-1], n_target_classes, use_relu=False)
+        return output_layer
+
+    def nn_conv_layers(self, input, filter_size, filter_num ):
+        layers = []
+        weights = []
+
+    def neural_network_model(self, data_image, n_target_classes, 
+            n_channels=1, 
+            s_filter1=5, n_filter1=16, 
+            s_filter2=5, n_filter2=36,
+            fc_size = 256 ):
+
+        layer_conv1, weights_conv1 = new_conv_layer(input=data_image, 
+            num_input_channels=n_channels,
+            filter_size= s_filter1,
+            num_filters= n_filter1,
+            use_pooling=False)
+
+        layer_conv2, weights_conv2 = new_conv_layer(input=layer_conv1,
+            num_input_channels= n_filter1,
+            filter_size= s_filter2,
+            num_filters= n_filter2,
+            use_pooling=False)
+
+        layer_flat, num_features = flatten_layer(layer_conv2)
+
+        layer_fc1 = new_fc_layer(input=layer_flat,
+            num_inputs= num_features,
+            num_outputs= fc_size,
+            use_relu=True)
+
+        layer_fc2 = new_fc_layer(input=layer_fc1,
+             num_inputs=fc_size,
+             num_outputs=n_target_classes,
+             use_relu=False)
+
+        return layer_fc2
 
     def __init__(self, level, receptive_field, data_location, 
                  batch_size=128, normalize_mode=True ):
@@ -69,6 +228,9 @@ class Model(object):
          
         input_norm_onehot_range = input_classes_max*2+1
         normalized_onehot = tf.one_hot(normalized_pos, input_norm_onehot_range)
+        
+        normalized_onehot_image = tf.reshape(
+                normalized_onehot, [-1, clip_size, input_norm_onehot_range,  1]) 
         normalized_onehot = tf.reshape(normalized_onehot, (-1, clip_size*input_norm_onehot_range))
         normalized_onehot = tf.cast(normalized_onehot, tf.float32)
                 
@@ -86,8 +248,8 @@ class Model(object):
                 target_normalized_onehot, (-1, target_norm_onehot_range) )
 
         #create list of perceptron levels with the number of corresponding nodes per level
-        n_nodes = [ (level+1)*400, (level+1)*100, (level+1)*50, ]
-        #n_nodes = [ 1024, 512, 256, ]
+        #n_nodes = [ (level+1)*400, (level+1)*100, (level+1)*50, ]
+        n_nodes = [ 1024, 512, 256, ]
 
 
         if normalize_mode == True:
@@ -103,8 +265,9 @@ class Model(object):
             nn_n_inputs = n_input_classes
             nn_n_targets = n_target_classes
 
-        logits = self.perceptron_nn(nn_inputs, nn_n_inputs, nn_n_targets, clip_size, n_nodes)
-        
+        #logits = self.perceptron_nn(nn_inputs, nn_n_inputs, nn_n_targets, clip_size, n_nodes)
+        logits = self.neural_network_model(normalized_onehot_image, target_norm_onehot_range)
+
         prediction = tf.nn.softmax(logits)
         prediction_class = tf.argmax(prediction, dimension=1)
 
@@ -136,6 +299,7 @@ class Model(object):
         self.normalized = normalized
         self.normalized_pos = normalized_pos
         self.normalized_onehot = normalized_onehot
+        self.normalized_onehot_image = normalized_onehot_image
 
         self.inputs_scaled = inputs_scaled
         self.target_normalized = target_normalized
@@ -174,17 +338,18 @@ class Model(object):
         for i in range(len(x)/2, len(x)/2 + 1, 10):
             feed_dict_test = {self.inputs: x[i:i+10,:],
                                self.target_class: ytrue_class[i:i+10] }
-            inp, mid, norm, norm_pos, norm_one, one, targ_c, in_scale, tar_nor, tar_nor_pos, tar_nor_pos_one, tar = self.sess.run(
+            inp, mid, norm, norm_pos, norm_one, one, targ_c, in_scale, tar_nor, tar_nor_pos, tar_nor_pos_one, tar, image = self.sess.run(
                     [self.inputs, self.middle, self.normalized, self.normalized_pos, 
                      self.normalized_onehot, self.onehot, self.target_class, self.inputs_scaled,
                      self.target_normalized, self.target_normalized_pos, 
-                     self.target_normalized_onehot, self.target],
+                     self.target_normalized_onehot, self.target, self.normalized_onehot_image],
                     feed_dict=feed_dict_test)
             print "inputs\n", inp
             print "middle value of receptive_field\n", mid
             print "normalized inputs\n", norm
             print "positive normalized\n", norm_pos
             print "onehot normalized inputs\n", norm_one
+            print "onehot image\n", image
             print "regular onehot inputs\n", one
             
             print "inputs scaled to next level\n", in_scale
