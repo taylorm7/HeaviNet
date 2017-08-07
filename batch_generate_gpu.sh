7b9764269dface6f7c2bf81c800660de26cef477
Taylor Sable McGough <taylorm7@nrlogin7.cluster>
2017-08-07 15:41:29 -0400

    batch_gen with GPU option added

---
+++ batch_generate_gpu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# At most XX minute of time
+#PBS -l walltime=02:15:00    
+
+# One core on any number of nodes
+#PBS -l procs=1,gpus=1
+
+#newriver cluster
+#PBS -W group_list=newriver  
+
+# queue
+#PBS -q normal_q
+
+#PBS -A heavinet
+
+# write output and error to the same file
+#PBS -j oe
+
+# Email when your job starts, completes, or aborts
+#PBS -M taylorm7@vt.edu
+#PBS -m ea
+
+module purge
+
+module load gcc/5.2.0 cuda/8.0.61 matlab/R2016b
+
+export PATH=$PATH:/home/taylorm7/opt_py35/bin
+export PYTHONUSERBASE=/home/taylorm7/opt_py35/newriver/python
+
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/taylorm7/opt/lib/pkgconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/taylorm7/opt/lib/
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/taylorm7/cuda/lib64/
+
+dot=$PBS_O_WORKDIR
+cd $dot
+pwd
+
+export dot=$dot
+./run_heavinet.sh generate beethoven_7.wav beethoven_7.wav 14
+echo "Code finished!"
+
