d6c19f5828946ef6bfb491b0e04dec07ebfb0e36
Sable <TaylorM7@vt.edu>
2017-08-13 13:47:52 -0400

    batch_iterate added

---
+++ batch_iterate.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# At most XX minute of time
+#PBS -l walltime=20:00:00    
+
+# One core on any number of nodes
+#PBS -l procs=1,gpus=1
+
+#newriver cluster
+#PBS -W group_list=newriver  
+
+# queue
+#PBS -q normal_q
+
+#PBS -A heavinet
+
+# write output and error to the same file
+#PBS -j oe
+
+# Email when your job starts, completes, or aborts
+#PBS -M taylorm7@vt.edu
+#PBS -m ea
+
+module purge
+
+module load gcc/5.2.0 cuda/8.0.61
+
+export PATH=$PATH:/home/taylorm7/opt_py35/bin
+export PYTHONUSERBASE=/home/taylorm7/opt_py35/newriver/python
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/taylorm7/cuda/lib64/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/taylorm7/opt/lib/
+
+dot=$PBS_O_WORKDIR
+cd $dot
+pwd
+
+export dot=$dot
+
+iterations=5
+
+song=beethoven_7.wav
+target=rand.wav
+receptive_field=32
+
+seed=$target
+name=$(echo $target | cut -f 1 -d '.')
+extension=$(echo $target | cut -f 2 -d '.')
+
+for ((i=0 ; i<iterations ; i++)); do
+	./run_heavinet.sh load $song $seed $receptive_field
+	./run_heavinet.sh generate $song $seed $receptive_field
+	./run_heavinet.sh create $song $seed $receptive_field
+
+	nextSeed="$name""_$(($i+1)).$extension"
+	cp ./data/$song.data/$seed.seed/song_r$receptive_field.wav ./data/songs/$nextSeed
+	seed=$nextSeed
+done
+
+echo "Code finished!"
+
