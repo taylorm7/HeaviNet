305e6fbab310d0ef5c04c2467e40b98af1fdeb88
Sable <TaylorM7@vt.edu>
2017-11-24 09:08:50 -0500

    a3; using target fx of 8000

--- set_fx.m
+++ set_fx.m
@@ -4,7 +4,7 @@ data_file = strcat(data_location, '/fx_info.mat');
 fx_offset = 25;
 fx_order = 3;
 use_exponential = 1;
-fx_target = 44100;
+fx_target = 8000;
 syms f positive
   
 if use_exponential
