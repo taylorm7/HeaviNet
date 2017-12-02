1d364bf6f6b06f6e3e8dde7f6b1238a2ba400761
Sable <TaylorM7@vt.edu>
2017-12-02 09:42:40 -0500

    Limiting song size to (-0.05, 0.05)

--- audio_format.m
+++ audio_format.m
@@ -39,8 +39,8 @@ frequencies = cell(n_levels,1);
 
 song =  hampel(song, 3, 0.5);
 song = sgolayfilt(song,5,41);
-%song_max = max(abs(song));
-%song = song./song_max;
+song_max = max(abs(song));
+song = (song./song_max)*0.05;
 
 for i = 1:n_levels
     
